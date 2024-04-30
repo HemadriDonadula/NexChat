28287ed6491758db00f5419df64b1352cb298b32
Hemadri Donadula <138286410+HemadriDonadula@users.noreply.github.com>
2024-04-30 16:32:20 +0530

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:494219880887:web:ef67921eb3042216fa6032";
+  static String apiKey = "AIzaSyD810iIw_idPDAo1tq72iN055zfeux12GI";
+  static String messagingSenderId = "494219880887";
+  static String projectId = "groupchat-9b5fe";
+  final primaryColor = const Color(0xFFee7b64);
+}
